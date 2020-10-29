a3dfe937c164eb9a3f04e085202d988b7d648ce0
LuK1337 <priv.luk@gmail.com>
2020-10-29 13:22:27 +0100

    Revert "klimt: Exclude lineage-sdk overlays from RRO"
    This reverts commit 7a323551b50419a8b6b566b26ba7800d37d0f764.
    
    Change-Id: I12f150b13686f8b91bb63e4ee728eb4016856aa5

--- device-common.mk
+++ device-common.mk
@@ -21,7 +21,6 @@ DEVICE_PATH := device/samsung/klimt-common
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(DEVICE_PATH)/overlay
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    device/samsung/klimt-common/overlay/lineage-sdk \
     device/samsung/klimt-common/overlay/hardware/samsung/AdvancedDisplay
 
 # Device uses high-density artwork where available
